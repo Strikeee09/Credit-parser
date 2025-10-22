d0de37ee3897909c21077f0bb371e9366ba6d175
Pratham Punikar <146797456+Strikeee09@users.noreply.github.com>
2025-10-23 00:16:20 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
   darkMode: ["class"],
@@ -91,4 +91,5 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+} 
+satisfies Config;
